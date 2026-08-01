df2e3d43fdc3c7a494f9b860a0001cc5fc251fc5
max93139 <max1234btc@gmail.com>
2026-08-01 21:08:36 +0300

    fix(admin): restore layout/index.tsx to resolve Next.js bundler cache issue

---
+++ apps/admin/src/components/layout/index.tsx
@@ -0,0 +1,7 @@
+import Sidebar from './sidebar';
+import Header from './header';
+import Main from './main';
+import Layout from './Layout';
+
+export { Sidebar, Header, Main, Layout };
+export default Layout;
